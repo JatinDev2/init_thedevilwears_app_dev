789413be76920d19533ac5049f4d9c2bd58715cd
Jatin <jainjatin468@gmail.com>
2023-07-19 19:15:22 +0530

    new listing

--- lib/screens/listing/new_listing/tags_screen.dart
+++ lib/screens/listing/new_listing/tags_screen.dart
@@ -66,76 +66,82 @@ class _TagScreenState extends State<TagScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        leading: IconButton(
-        icon: Icon(Icons.arrow_back_ios_new_outlined,
-          color: Colors.black,
-          size: 26,),
-          onPressed: (){
-          Navigator.of(context).pop(selectedOptions);
-          },
-        ),
-        title: Text(
-          "Add what you’re looking for",
-          style: const TextStyle(
-            fontFamily: "Poppins",
-            fontSize: 16,
-            fontWeight: FontWeight.w500,
-            color: Color(0xff0f1015),
-            height: 20 / 16,
+    return WillPopScope(
+      onWillPop: ()async{
+        Navigator.of(context).pop(selectedOptions);
+        return false;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios_new_outlined,
+            color: Colors.black,
+            size: 26,),
+            onPressed: (){
+            Navigator.of(context).pop(selectedOptions);
+            },
           ),
-          textAlign: TextAlign.left,
+          title: Text(
+            "Add what you’re looking for",
+            style: const TextStyle(
+              fontFamily: "Poppins",
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+              color: Color(0xff0f1015),
+              height: 20 / 16,
+            ),
+            textAlign: TextAlign.left,
+          ),
         ),
-      ),
-      body: Container(
-        margin: EdgeInsets.all(10.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              margin: EdgeInsets.all(5.0),
-              child: Text(
-                "Gender",
-                style: const TextStyle(
-                  fontFamily: "Poppins",
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600,
-                  color: Color(0xff2d2d2d),
-                  height: 24 / 16,
+        body: Container(
+          margin: EdgeInsets.all(10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                margin: EdgeInsets.all(5.0),
+                child: Text(
+                  "Gender",
+                  style: const TextStyle(
+                    fontFamily: "Poppins",
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: Color(0xff2d2d2d),
+                    height: 24 / 16,
+                  ),
+                  textAlign: TextAlign.left,
                 ),
-                textAlign: TextAlign.left,
               ),
-            ),
-            Wrap(
-              spacing: 5.0,
-              runSpacing: 5.0,
-              children: gender.map((option) => buildOption(option)).toList(),
-            ),
-            SizedBox(height: 30,),
+              Wrap(
+                spacing: 5.0,
+                runSpacing: 5.0,
+                children: gender.map((option) => buildOption(option)).toList(),
+              ),
+              SizedBox(height: 30,),
 
-            Container(
-              margin: EdgeInsets.all(5.0),
-              child: Text(
-                "Occasion",
-                style: const TextStyle(
-                  fontFamily: "Poppins",
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600,
-                  color: Color(0xff2d2d2d),
-                  height: 24 / 16,
+              Container(
+                margin: EdgeInsets.all(5.0),
+                child: Text(
+                  "Occasion",
+                  style: const TextStyle(
+                    fontFamily: "Poppins",
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: Color(0xff2d2d2d),
+                    height: 24 / 16,
+                  ),
+                  textAlign: TextAlign.left,
                 ),
-                textAlign: TextAlign.left,
               ),
-            ),
-            Wrap(
-              spacing: 5.0,
-              runSpacing: 5.0,
-              children: occasion.map((option) => buildOption(option)).toList(),
-            ),
+              Wrap(
+                spacing: 5.0,
+                runSpacing: 5.0,
+                children: occasion.map((option) => buildOption(option)).toList(),
+              ),
 
-          ],
+            ],
+          ),
         ),
       ),
     );
